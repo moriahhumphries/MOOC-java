217273ab8ad0311efecc3d8155cf09d121c8223e
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-07 16:00:37 -0500

    Add files via upload

---
+++ CheckingTheAge.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class CheckingTheAge {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int userAge = Integer.valueOf(scan.nextLine());
+
+        if (userAge >= 0 && userAge <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+
+    }
+}
