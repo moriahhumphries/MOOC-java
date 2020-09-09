009c04851b71370ae2e7915b9315e65d33d8e8c1
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-09 16:02:22 -0500

    Add files via upload

---
+++ ComparingNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userNum1 = Integer.valueOf(scanner.nextLine());
+        int userNum2 = Integer.valueOf(scanner.nextLine());
+        
+        if (userNum1 == userNum2) {
+            System.out.println(userNum1 + " is equal to " + userNum2);
+        } else if (userNum1 > userNum2) {
+            System.out.println(userNum1 + " is greater than " + userNum2);
+        } else {
+            System.out.println(userNum1 + " is smaller than " + userNum2);
+        }
+        
+        
+        
+
+    }
+}
