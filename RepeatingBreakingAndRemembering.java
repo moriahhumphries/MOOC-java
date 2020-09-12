668fb61d3645afdb6b562f5b4adc9710b5c28b09
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-12 15:58:52 -0500

    Add files via upload

---
+++ RepeatingBreakingAndRemembering.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+        
+        // This exercise is worth five exercise points, and it is 
+        // gradually extended part by part.
+        
+        // If you want, you can send this exercise to the server
+        // when it's just partially done. In that case the server will complain about 
+        // the parts you haven't done, but you'll get points for the finished parts.
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        
+        int sum = 0;
+        int numbers = 0;
+        int oddNum = 0;
+        int evenNum = 0;
+        
+        while (true) {
+            int userNum = Integer.valueOf(scanner.nextLine());
+            if (userNum == -1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            
+            if (userNum % 2 == 0) {
+                evenNum++;
+                
+            } else {
+                oddNum++;
+            }
+            
+            sum += userNum;
+            numbers++;
+        }
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average: " + ((double) sum / numbers));
+        System.out.println("Even: " + evenNum);
+        System.out.println("Odd: " + oddNum);
+    }
+}
