f6352f046c06e92665d60e88f41db3e3f274666d
Moriah Humphries <moriah.humphries@gmail.com>
2020-10-21 13:59:11 -0500

    Add files via upload

---
+++ ArrayPrinter.java
@@ -0,0 +1,23 @@
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        // Write some code in here
+        int index = 0;
+        while (index < array.length - 1) {
+            int number = array[index];
+            System.out.print(number + ", ");
+            index++;
+            
+        }
+
+        System.out.println(array[index]);
+    }
+}
+
