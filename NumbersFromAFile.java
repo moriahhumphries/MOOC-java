20f6be4b9312ee7ef84e06dcbc87149fc1da2e7e
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-04 16:26:18 -0600

    Add files via upload

---
+++ NumbersFromAFile.java
@@ -0,0 +1,35 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+        int totalNum = 0;
+        
+        try ( Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+               int number = Integer.valueOf(fileReader.nextLine());
+                if (number >= lowerBound && number <= upperBound) {
+                   totalNum++;
+                }
+
+            }
+
+           
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed");
+        }
+
+        System.out.println("Numbers: " + totalNum);
+    }
+
+}
